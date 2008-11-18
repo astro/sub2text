8ba6398837304d8d9e79d7606946e83b4c3b4d05
Astro <astro@spaceboyz.net>
2008-11-18 04:10:03 +0100

    pubsub: duplicate item detection

--- pubsub.erl
+++ pubsub.erl
@@ -4,7 +4,11 @@
 
 -include_lib("exmpp.hrl").
 
+-record(seen_item, {jni, last}).
+
 start() ->
+    mnesia:create_table(seen_item, [{disc_copies, [node()]},
+				    {attributes, record_info(fields, seen_item)}]),
     client:register_listener(?MODULE).
 
 stop() ->
@@ -27,20 +31,44 @@ handle_event(_, []) ->
      ok;
 handle_event(JID, [#xmlel{name = items} = Items | Els]) ->
     Node = exmpp_xml:get_attribute(Items, node, ""),
-    case subscriptions:get_subscribers_of(JID, Node) of
+    {atomic, NewItems} =
+	mnesia:transaction(
+	  fun() ->
+		  mnesia:write_lock_table(seen_item),
 
-	[] ->
-	    unsubscribe(JID, Node);
-
-	Users ->
-	    Msg1 = exmpp_message:chat(),
-	    Msg2 = Msg1#xmlel{children =
-			      item_to_msg:transform_items(JID, Items)},
-	    lists:foreach(
-	      fun(User) ->
-		      client:send(exmpp_stanza:set_recipient(Msg2,
-							     User))
-	      end, Users)
+		  lists:filter(
+		    fun(Item) ->
+			    Id = exmpp_xml:get_attribute(Item, id, ""),
+			    JNI = {JID, Node, Id},
+			    case mnesia:read({seen_item, JNI}) of
+				[] ->
+				    mnesia:write(#seen_item{jni = JNI, last = current_timestamp()}),
+				    true;
+				_ ->
+				    io:format("Skipping ~p~n", [JNI]),
+				    false
+			    end
+		    end, exmpp_xml:get_elements(Items, item))
+	  end),
+    if
+	NewItems =:= [] ->
+	    ignore;
+	true ->
+	    case subscriptions:get_subscribers_of(JID, Node) of
+		
+		[] ->
+		    unsubscribe(JID, Node);
+		
+		Users ->
+		    Msg1 = exmpp_message:chat(),
+		    Msg2 = Msg1#xmlel{children =
+				      item_to_msg:transform_items(JID, NewItems)},
+		    lists:foreach(
+		      fun(User) ->
+			      client:send(exmpp_stanza:set_recipient(Msg2,
+								     User))
+		      end, Users)
+	    end
     end,
     handle_event(JID, Els);
 handle_event(JID, [_ | Els]) ->
@@ -74,3 +102,7 @@ unsubscribe(JID, Node) ->
 	     []}]}]},
     client:send_recv(Iq),
     ok.
+
+current_timestamp() ->
+    {M, S, _} = now(),
+    M * 1000000 + S.
